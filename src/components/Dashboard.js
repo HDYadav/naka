fedfc548af4ded2c5a37e9f8e19c76d04eb4f9eb
Mohd Aadil Rana <aadilranajmi@gmail.com>
2024-05-02 00:35:51 +0530

    fixed the layout fwhen sidebar is open or close

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,14 @@
 import React from "react";
 import LayoutHOC from "./LayoutHOC";
+import { useSelector } from "react-redux";
 
 const Dashboard = () => {
-  return <div className="p-8 ml-44">
-  <h1>Welcome to Dashboard</h1>
-  </div>;
+  const isSidebarOpen = useSelector((store) => store.commanSlice.isSidebarOpen);
+  return (
+    <div className={`p-8 ${isSidebarOpen ? "w-[calc(100%-300px)] ml-[300px]" : "w-full"}`}>
+      <h1>Welcome to Dashboard</h1>
+    </div>
+  );
 };
 
 export default LayoutHOC(Dashboard);
--- src/components/Leftmenu.js
+++ src/components/Leftmenu.js
@@ -26,7 +26,7 @@ const Leftmenu = (props) => {
   };
 
   return (
-    <div>
+    <>
       {isSidebarOpen && (
         <div className="sidebar fixed top-0 bottom-0 lg:left-0 p-2 w-[300px] overflow-y-auto text-center bg-gray-900">
           <div className="text-gray-100 text-xl">
@@ -96,7 +96,10 @@ const Leftmenu = (props) => {
               </span>
             </div>
           </Link>
-          <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white" onClick={handleSignOut}>
+          <div
+            className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
+            onClick={handleSignOut}
+          >
             <RiLogoutCircleRLine />
             <span className="text-[15px] ml-4 text-gray-200 font-bold">
               Logout
@@ -104,7 +107,7 @@ const Leftmenu = (props) => {
           </div>
         </div>
       )}
-    </div>
+    </>
   );
 };
 
