49d9c01cc8e4a6f8667093d35adf92eaaf7738ed
Mohd Aadil Rana <aadilranajmi@gmail.com>
2024-05-02 02:15:19 +0530

    created the create job page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className="bg-gray-800 text-white text-center py-4 fixed bottom-0 w-full">
+    <footer className="text-xs sm:text-base bg-gray-800 text-white text-center py-4 fixed bottom-0 w-full ">
       &copy; 2024 Admin Dashboard. All rights reserved.
     </footer>
   );
--- src/components/Jobs/CreateJob.js
+++ src/components/Jobs/CreateJob.js
@@ -1,66 +1,116 @@
-import React from "react";
+import React, { useState } from "react";
 import LayoutHOC from "../LayoutHOC";
 import { useSelector } from "react-redux";
 
 const CreateJob = () => {
-    const isSidebarOpen = useSelector((store) => store.commanSlice.isSidebarOpen);
+  const isSidebarOpen = useSelector((store) => store.commanSlice.isSidebarOpen);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    address: "",
+    carrierType: "", 
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form submitted:", formData);
+    // Perform form submission logic here (e.g., API call)
+  };
   return (
     <div
-      className={`p-8 ${
+      className={`p-8 pb-20 ${
         isSidebarOpen ? "w-[calc(100%-300px)] ml-[300px]" : "w-full"
       }`}
     >
-      <div className="mb-4 px-4 py-2">
-        <input
-          type="text"
-          placeholder="Search by name or email"
-          className="px-4 py-2 border rounded"
-        />
-      </div>
-
-      <div className="px-4 py-2">
-        <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3">
-          <div className="mb-5 flex flex-col">
-            <label
-              htmlFor="large-input1"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Large input 1
-            </label>
-            <input
-              type="text"
-              id="large-input1"
-              className="block px-2 py-2 w-full text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            />
-          </div>
-          <div className="mb-5 flex flex-col">
-            <label
-              htmlFor="large-input2"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Large input 2
-            </label>
-            <input
-              type="text"
-              id="large-input2"
-              className="block w-full px-4 py-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            />
-          </div>
-          <div className="mb-5 flex flex-col">
-            <label
-              htmlFor="large-input3"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Large input 3
-            </label>
-            <input
-              type="text"
-              id="large-input3"
-              className="block w-full px-4 py-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            />
-          </div>
+      <h1 className="text-2xl font-bold mb-4">Create Jobs</h1>
+      <form onSubmit={handleSubmit} className=" flex flex-col md:grid sm:grid-cols-2 gap-3">
+        <div>
+          <label htmlFor="name" className="block text-gray-700">
+            Name:
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+            value={formData.name}
+            onChange={handleInputChange}
+          />
         </div>
-      </div>
+        <div>
+          <label htmlFor="email" className="block text-gray-700">
+            Email:
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+            value={formData.email}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="phone" className="block text-gray-700">
+            Phone:
+          </label>
+          <input
+            type="tel"
+            name="phone"
+            id="phone"
+            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+            value={formData.phone}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="address" className="block text-gray-700">
+            Address:
+          </label>
+          <input
+            type="text"
+            name="address"
+            id="address"
+            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+            value={formData.address}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="carrierType" className="block text-gray-700">
+            Carrier Type:
+          </label>
+          <select
+            name="carrierType"
+            id="carrierType"
+            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+            value={formData.carrierType}
+            onChange={handleInputChange}
+          >
+            <option value="">Select a type</option>
+            <option value="truck">Software Developer</option>
+            <option value="ship">Frontend Developer</option>
+            <option value="airplane">Backend Developer</option>
+          </select>
+        </div>
+        <div className="col-span-2 mt-3">
+          <button
+            type="submit"
+            className=" bg-blue-600 text-white py-2 px-6 rounded-md hover:bg-blue-700 focus:outline-none focus:bg-blue-800"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
